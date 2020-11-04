0a8a76a17d2d1a82064f5c26b76e3342574a79d8
Mona Lisa <tes@example.co.jp>
2020-11-04 23:36:07 +0900

    [add]ファイル

---
+++ django_blog/app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import get_object_or_404,redirect,render
+from django.contrib.auth.models import User
+
+# Create your views here.
+def index(request):
+    return render(request, 'app/index.html')
+
+def users_detail(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    params = {
+        'user':user,
+    }
+    return render(request,'app/users_detail.html',params)
